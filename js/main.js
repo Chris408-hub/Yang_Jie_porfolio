d4dc095073652557f4e7d469bcb3ba1fa8dd841e
Chris408-hub <112998909+Chris408-hub@users.noreply.github.com>
2023-12-10 01:59:56 -0500

    adding hamburger menu

--- js/main.js
+++ js/main.js
@@ -3,16 +3,15 @@
 
   //hamburger menu
   
-  const hamButton = document.querySelector('#button');
-  const burgerCon = document.querySelector('#burger-con');
-  
-	function hamburgerMenu() {
-		burgerCon.classList.toggle('slide-toggle');
-		hamButton.classList.toggle('expanded');
-	};
+const hamButton = document.querySelector('.hamburger-button');
+const burgerCon = document.querySelector('#burger-con');
 
+function hamburgerMenu() {
+    burgerCon.classList.toggle('slide-toggle');
+    hamButton.classList.toggle('expanded');
+}
 
-  hamButton.addEventListener('click', hamburgerMenu, false);
+hamButton.addEventListener('click', hamburgerMenu, false);
 
 
 
