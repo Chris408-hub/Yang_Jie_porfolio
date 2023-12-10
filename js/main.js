d443476409a2ff5e9007cacfd772c91576a11921
Jie Yang <112998909+Chris408-hub@users.noreply.github.com>
2023-12-10 16:09:45 -0500

    Update show and hide in main.js

--- js/main.js
+++ js/main.js
@@ -13,6 +13,44 @@ function hamburgerMenu() {
 
   hamButton.addEventListener('click', hamburgerMenu, false);
   
+// show and hide
+    const allButton = document.querySelector('#all');
+    const codeButton = document.querySelector('#code');
+    const designButton = document.querySelector('#design');
+
+    const allProjects = document.querySelectorAll('.gallery-box');
+    const codeProjects = document.querySelectorAll('.code');
+    const designProjects = document.querySelectorAll('.design');
+
+    function hideAllProjects() {
+        allProjects.forEach(project => {
+            project.style.display = 'none';
+        });
+    }
+
+    function showAllProjects() {
+        allProjects.forEach(project => {
+            project.style.display = 'block';
+        });
+    }
+
+    function showCodeProjects() {
+        hideAllProjects();
+        codeProjects.forEach(project => {
+            project.style.display = 'block';
+        });
+    }
+
+    function showDesignProjects() {
+        hideAllProjects();
+        designProjects.forEach(project => {
+            project.style.display = 'block';
+        });
+    }
+
+    allButton.addEventListener('click', showAllProjects);
+    codeButton.addEventListener('click', showCodeProjects);
+    designButton.addEventListener('click', showDesignProjects);
   
 // video 
 
